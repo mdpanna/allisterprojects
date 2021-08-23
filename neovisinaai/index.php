299f456952dcd0cf0e0e844c8c8154338da90f8c
mdpanna <75739631+mdpanna@users.noreply.github.com>
2021-08-23 21:22:56 +0600

    Create index.php

---
+++ neovisinaai/index.php
@@ -0,0 +1,345 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KVPTW42');</script>
+    <!-- End Google Tag Manager -->
+    
+    
+    <meta charset="utf-8">
+    <title>Neo Vision AI - The Most Powerful and Precise HR Analytics and Recruiting Technology In The World.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Neo Vision AI - The Most Powerful and Precise HR Analytics and Recruiting Technology In The World." />
+    <meta name="keywords" content="Employee, Salaried, Benefits, Applying For Unemployment, Unemployment Stimulus, My Benefits, Employer On Demand, Paychex Flex, Adp, Adp Workforce, Intuit Payroll, Exl Payroll, Oasis Paychex, Heartland Checkview, Gusto Pay,sba Grant, Sba Loan, Neo Vision Ai, NEO VISION INC  " />
+    
+    <link rel="canonical" href="https://www.neovisionai.tech" />
+    <meta property="og:title" content="neovisionai" />
+    <meta property="og:description" content="Neo Vision AI - The Most Powerful and Precise HR Analytics and Recruiting Technology In The World." />
+    <meta property="og:type" content="Website" />
+    <meta property="og:url" content="https://www.neovisionai.tech" />
+    <meta property="og:image" content="https://neovisionai.tech/img/logo.png" />
+    <meta property="og:site_name" content="neovisionai" />
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="neovisionai">
+    <meta name="twitter:description" content="Neo Vision AI - The Most Powerful and Precise HR Analytics and Recruiting Technology In The World.">
+    <meta name="author" content="neovisionai">
+
+
+    <link rel="icon" href="img/logo.png">
+
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <meta name="theme-color" content="#fafafa">
+    
+    
+    <meta name="google-site-verification" content="7PRcjNFh582p8hKzrcP_yz-c8WCvQRaxf-rrzbsWrQI" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DT8F8G4BDL"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-DT8F8G4BDL');
+    </script>
+
+</head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KVPTW42"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+    <header class="header-part">
+
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="none" class="img-fluid"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="myicon"><i class="fas fa-bars"></i></span>
+                </button>
+
+                <div class="cust_collapse">
+                    <a class="close-btn"><i class="fas fa-times"></i></a>
+                    <ul class="navbar-nav ml-auto">
+
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about_us">ABOUT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#service_part">SERVICES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#work_part">Work</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="blog.php">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="terms-conditions.php">Terms & Conditions</a>
+                        </li>
+                        <li class="nav-item last_item">
+                            <a class="nav-link" href="https://neovision.cc/login" target="_blank">Login</a>
+                        </li>
+
+
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section class="banner-part">
+        <div class="overlay">
+            <div class="container">
+                <div class="content-box">
+                    <div class="box">
+                        <h1>NEO VISION INC.</h1>
+                        <h4>The latest in artificial intelligence and innovation has arrived</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="about-part comm_padd" id="about_us">
+        <img src="img/section_bg_img1.svg" alt="none" class="img-fluid section_bg_img1">
+
+        <div class="container">
+            <div class="content-box">
+
+                <div class="row">
+                    <div class="col-lg-6 col-12">
+                        <div class="box">
+                            <div class="title-box">
+                                <h5>ABOUT NEO VISION INC.</h5>
+                                <h3>NEO VISION IS AN ARTIFICIAL INTELLIGENCE THAT ANALYZES THE PERSONALITY DNA OF MILLIONS WITHIN MINUTES.</h3>
+                            </div>
+                            <p>Meet Neo Vision Inc. — Using this artificial intelligence you can map your target market more accurately than ever before. Enhance workforce development in real-time by matching talent with the areas of focus that align with their skillset driving efficiency and retention. The ability to leverage cognitive and skill insights across various verticals is continuing to make Neo Vision the leader in innovation for understanding individuals personality DNA.
+
+                            </p>
+                            <p>Understand new dimensions of your target market and your workforce like never before. Not only does Neo Vision enable you to source and screen talent and clients alike with anti-bias AI, it also provides and in- depth understanding of your current talent and client base.
+
+                            </p>
+                            <p>During a time where making calculated decisions rapidly in order to realign with the changing times Neo Vision provides market insights that truly allow cohesion both in and outside of your organization.
+
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <div class="box">
+                            <img src="img/sct1_img.svg" alt="none" class="img-fluid">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="work_place-part comm_padd" id="service_part">
+        <img src="img/section_bg_img2.svg" alt="none" class="img-fluid section_bg_img2">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title-box">
+                        <h5>PARADIGM SHIFT
+                        </h5>
+                        <h3>TRANSFORM YOUR APPROACH TO MARKETING AND WORKPLACE INCLUSIVITY
+                        </h3>
+                    </div>
+                    <div class="content-box">
+                        <p>Get highly qualified, preselected passive candiates who are culturally aligned and have the skills required for the role neeed in real time.
+
+                        </p>
+                        <p>Receive in depth insights on specifically targeted decision makers within your niche in real time.
+
+                        </p>
+                        <p>Reduce your cost-to-hire by nearly 60% while streamlining your sales cycle and talent management cycle simultaneously.
+
+                        </p>
+
+                        <div class="row mt-5">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="img-box"> <img src="img/work_place_img/img.svg" alt="none" class="img-fluid"></div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="box">
+                                            <h4>SCREENING</h4>
+                                            <p>Identify with greater than 84% accuracy the personality characteristics of your market and talent in real-time.</p>
+                                            <p>Neo Vision, helps you find better talent, faster. Screen through thousands of candidates in minutes with our skills tests, and find the perfect fit for your role.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="box">
+                                            <h4>INSTANT MARKET ANALYSIS</h4>
+                                            <p>Reduce cost to hire by nearly 60% while comprhensively revealing the insights necessary to realign your company culture to your most marketable.</p>
+
+                                            <h4>DISCOVER THE BEST TALENT, INSTANTLY</h4>
+
+                                            <p>Powerful AI Neo Vision automatically shortlists and ranks the highest-scoring candidates</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="img-box"> <img src="img/work_place_img/img5.png" alt="none" class="img-fluid"></div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="img-box"> <img src="img/work_place_img/img2.png" alt="none" class="img-fluid"></div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="box">
+                                            <h4>ENGAGING </h4>
+                                            <p>Increase retention and prductivity rates by as much as 2.5x with powerful AI that helps you ensure orgnaiztional continuity. Increase your client conversion rate exponentially while targeting their desired learning habits strategically based on AI analysis.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="box">
+                                            <h4>COMPREHENSIVE CULTIVATION</h4>
+                                            <p>Identify the next prospect ready to close while upskilling your sales team and entire workforce in real time. Upskill your workforce in real time with beneficial future development courses that will help them be more productive.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="img-box"> <img src="img/work_place_img/img4.png" alt="none" class="img-fluid"></div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="sct1-part comm_padd">
+        <img src="img/section_bg_img3.png" alt="none" class="img-fluid section_bg_img3">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title-box">
+                        <h3>PERFIT- WORK FROM HOME</h3>
+                    </div>
+                    <div class="content-box">
+                        <p>Gain powerful insights into how your team works from home and make critical business decisions with confidence. Identify synergy in team dynamics and get realistic recommendations for improving them, including incentives for motivating emplyees, suggesitons for work environments, and guides for comunication style.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="sct1-part comm_padd">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title-box">
+                        <h3>THE SCIENCE BEHIND THE ALGORITHM
+                        </h3>
+                    </div>
+                    <div class="content-box">
+                        <p>Neo Vision is accurate, universal and unbiased, thanks to the robust machine learning, sophisticated artificial intelligence, and complex coding that powers it. Color theory and Natural Language Processing (NLP) are the two major principles upon which our solutions are built, as is realizing subconscious psychological functioning. These models do not incorporate data that induces subjectivity (gender, race, ethnicity, facial expressions, and contours), so they therefore deliver outcomes that are proven to be accurate and guaranteed to be bias-free.
+
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sct1-part comm_padd" id="work_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content-box">
+                        <h2>ARE YOU READY TO IMPLEMENT A MARKETING AND HR SOLUTION THAT WILL FOREVER CHANGE THE WAY YOU ACQUIRE, ENGAGE AND ENRICH YOUR RELATIONSHIPS WITH YOUR CLIENTS AND PERSONNEL ALIKE?
+                        </h2>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+    <section class="contact-part comm_padd">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title-box">
+                        <h5>LET'S CONNECT
+                        </h5>
+                        <h3>FEEL FREE TO GIVE US A CALL OR SEND US AN EMAIL WITH YOUR QUESTIONS OR COMMENTS.
+                        </h3>
+                        <p>We would love to hear from you, whether to schedule a demo or answer any questions you may have.
+
+                        </p>
+                    </div>
+                    <div class="content-box">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <div class="box">
+
+                                   <div class="_form_11"></div><script src="https://morpheustech.cc/f/embed.php?id=11" type="text/javascript" charset="utf-8"></script>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<?php require_once('inc/footer.php'); ?>
