08e74358051a269613e526fcb5493819a50e398a
mdpanna <75739631+mdpanna@users.noreply.github.com>
2021-08-23 20:51:37 +0600

    index.php

---
+++ meinexecutive/index.php
@@ -0,0 +1,391 @@
+<?php
+require_once("lib/classes.php");
+?>
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-MS8R87Q');
+
+    </script>
+    <!-- End Google Tag Manager -->
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMWJBCLF8V"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-NMWJBCLF8V');
+
+    </script>
+
+    <meta charset="utf-8">
+    <title>Meinexecutive | Welcome to Mein Executive Talent</title>
+    <meta name="description" content="We provide recruiting services for every level of position focusing on semiconductor, security, and optical communications companies in Silicon Valley." />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="keywords" content="Mein Executive, Meinexecutive, recruiting services, companies in Silicon Valley, Recruiting and Consulting, John C. Mein, john Meinexecutive, 
+John C. Mein - President - Mein Executive Talent,  Mein Executive Talent, Business Development Manager, executive talent solutions  " />
+    <link rel="canonical" href="https://www.meinexecutive.com" />
+    
+    <meta property="og:title" content="meinexecutive" />
+    <meta property="og:description" content="We provide recruiting services for every level of position focusing on semiconductor, security, and optical communications companies in Silicon Valley." />
+    <meta property="og:type" content="Recruiting and Consulting" />
+    <meta property="og:url" content="https://www.meinexecutive.com" />
+    <meta property="og:image" content="https://meinexecutive.com/img/logo.png" />
+    <meta property="og:site_name" content="meinexecutive" />
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="meinexecutive">
+    <meta name="twitter:description" content="We provide recruiting services for every level of position focusing on semiconductor, security, and optical communications companies in Silicon Valley.">
+    <meta name="author" content="John C. Mein">
+
+    <?php require_once("inc/header.php"); ?>
+
+    <meta name="google-site-verification" content="mqseFWi8OwJQ9okkZLSJUg8ppmjOYh2K6gXgobrnEOU" />
+
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS8R87Q" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <header class="header-part">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="top-box">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="box">
+                                    <button class="navbar_toggler_show-btn">
+                                        <span class="myicon"><i class="fas fa-bars"></i></span>
+                                    </button>
+                                    <button class="navbar_toggler_close-btn">
+                                        <span class="myicon"><i class="fas fa-times"></i></span>
+                                    </button>
+                                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="none" class="img-fluid"></a>
+
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="upload-box">
+                                    <div class="btn-box">
+
+                                        <a class="cust_button cust_button-mat btn--5" data-toggle="modal" data-target="#uploadcvModal">
+                                            <div class="cust_btn-text"><span>UPLOAD CV</span> <i class="fas fa-long-arrow-alt-right"></i></div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-12">
+                    <div class="bottom-box">
+                        <div class="cust_collapse ">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="box">
+                                        <ul class="navbar-nav ml-auto">
+
+                                            <?php require_once('inc/navbars.php'); ?>
+
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <section class="banner-part">
+        <div class="overlay">
+            <div class="container">
+                <div class="content-box">
+                    <div class="main-box">
+                        <div class="row">
+                            <div class="col-lg-2 col-3">
+                                <div class="img-box">
+                                    <img src="img/ico-white-semiconductors-components.svg" alt="none" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="col-lg-10 col-9">
+                                <div class="details-box">
+
+                                    <h1>Your Semiconductor Specialists</h1>
+                                    <h4>In an era of unprecedented change, we place people into the microelectronics jobs that shape our world.</h4>
+                                    <div class="btn-box">
+                                        <a class="cust_button cust_button-mat btn--5" data-toggle="modal" data-target="#contact_form_btn">
+                                            <div class="cust_btn-text"><span>LOOKING FOR A JOB?</span></div>
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+    <section class="sect1-part">
+        <div class="container">
+
+            <div class="content-box">
+                <h3><span><i class="fas fa-circle"></i></span> Semiconductor recruitment is where we started, and now we're one of the best.</h3>
+                <p>Technology is evolving at a rapid pace. And over the last several years, we have been placing candidates into components jobs across the leading technology companies of the world. </p>
+                <p>From the Design Engineers creating the latest prototypes and the Sales and Marketing Leaders taking the product to market to the Project Managers looking over the whole business, we can cover all bases within the sector, whether you're looking for semiconductor recruiters or you're a job hunter. </p>
+                <h4>Why choose us as your semiconductor recruiters?</h4>
+                <p>Our success comes from striving to serve the world’s technology industries with our market-leading recruitment services. Respect and integrity are core to our approach, as is our expert knowledge gleaned from years of matching excellent candidates with the right technical and commercial roles.
+
+                </p>
+                <p>With a view to the future, we know that this industry is constantly changing. We understand the complex job requirements that might not be commonplace yet and our extensive background in the industry provides us with a credible reputation for placing talent across the IC design and manufacturing</p>
+
+
+
+                <h4>Semiconductor industries we currently recruit in
+                </h4>
+
+                <div class="row">
+                    <div class="col-md-6 col-12">
+                        <div class="box">
+                            <ul>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Components Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Software Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Sales Director Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>IC Tester Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Equipment Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Deep Learning Engineering Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Verification Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Research Engineer Jobs</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-12">
+                        <div class="box">
+                            <ul>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Microelectronics Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Process Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Field Application Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>ASIC Design Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Yield Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>FPGA Design Engineer Jobs</a></li>
+                                <li><a href="#"><span><i class="fas fa-circle"></i></span>Software Architecture Engineer Jobs</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <p>The areas we cover start with Design and flow all the way through testing to sales and marketing.</p>
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    <section class="sect2-part ">
+        <div class="overlay comm_padd">
+            <div class="container">
+                <div class="content-box">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <div class="box">
+                                <h4>A new era of creativity and innovation is on the horizon, as the semiconductor industry turns its focus to developing new technology, rather than scaling, porting and integration.</h4>
+                                <p>Find out more about the future of the Semiconductor sector with our latest industry insights report.</p>
+                                <div class="btn-box">
+                                    <a class="cust_button cust_button-mat btn--5">
+                                        <div class="cust_btn-text"><span>READ MORE</span> <i class="fas fa-long-arrow-alt-right"></i></div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section class="sect3-part comm_padd">
+        <div class="container">
+
+            <div class="content-box">
+
+                <div class="row m-auto">
+                    <div class="col-lg-8">
+                        <div class="box">
+                            <i class="far fa-envelope"></i>
+                            <h4>We can find you world-class candidates within the Semiconductor sector.</h4>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="box">
+                            <div class="btn-box">
+                                <a class="cust_button cust_button-mat btn--5" data-toggle="modal" data-target="#contact_form_btn">
+                                    <div class="cust_btn-text"><span>CONTACT US TODAY</span> <i class="fas fa-long-arrow-alt-right"></i></div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+    <section class="testimonial-part comm_padd">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="left-box">
+                        <h3>Testimonial</h3>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="owl-carousel">
+                        <div class="right-box">
+                            <p><i class="fas fa-quote-left"></i>Mein Executive Talent did a superb for us in our search for a major accounts director of sales for our semiconductor company.  John Mein was able to quickly supply a large number of candidates that met our profile.  He then prescreened through personal interviews to find several candidates that in the end made our final choice quite difficult.  John consulted with me on each candidate after their interviews with the company. Our finalist did accept the position and is working out very well. John was there throughout the entire process providing constant communication and feedback to both sides to expedite the entire hiring process.  I would highly recommend Mein Executive talent for your talent search and I would not hesitate to use their services again.    </p>
+                            <label> – Brian, VP of Strategic Sales, public Silicon Valley Semiconductor firm</label>
+                        </div>
+                        <div class="right-box">
+                            <p><i class="fas fa-quote-left"></i>I have known John at Mein Executive Talent for several years even before he started recruiting. I recently retained him to find two sales account managers for our firm. He does the background work to find suitable, interested, and affordable candidates for us.  He participated in Zoom interview calls as we screened the candidates.  As a result, we were quickly able to hire the right people who have turned out to be great employees! John is technical, highly connected, located in Silicon Valley, and very responsive to our needs.  I highly recommend his company and services.</p>
+                            <label> -Doug, President of a Western multi-state Manufacturer’s Representative company.</label>
+                        </div>
+                        <div class="right-box">
+                            <p><i class="fas fa-quote-left"></i>John at Mein Executive Talent has done a superb job of sourcing and vetting system engineers and sales engineers for me.  He does the background work to find suitable, interested, and affordable candidates for us.I have worked with John at multiple companies doing multiple searches across the US.  John is technical, highly connected, located in Silicon Valley, and very responsive to our needs. I highly recommend his company and services.</p>
+                            <label> –Scott, VP of Business Development at a Network Security firm</label>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section class="blog-part comm_padd">
+        <div class="container">
+            <div class="title-box">
+                <h3>Blog</h3>
+
+            </div>
+            <div class="content-box">
+                <div class="row">
+                    <?php
+                      
+                        $sel="SELECT * FROM $blog ORDER BY id ASC LIMIT 3";
+                        $query=$db2->select($sel);
+                        if($query){
+                             while($data=$query->fetch_assoc()){
+
+                    
+                    ?>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="box">
+                            <img src="img/blog/<?php echo $data['image']; ?>" alt="none" class="img-fluid">
+                            <div class="details-box">
+                                <h4><a href="blog-details.php?id=<?php echo $data['id']; ?>"><?php echo $data['title']; ?></a></h4>
+                                <label><i class="far fa-calendar-alt"></i> <?php echo $data['date']; ?></label>
+                                <p><?php echo limit_words($data['para'],40); ?>...<a href="blog-details.php?id=<?php echo $data['id']; ?>">See More</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+
+                             }
+
+                        }
+                    ?>
+
+
+
+                </div>
+
+                <div class="btn-box">
+                    <div class="btn-box">
+                        <a href="blog.php">
+                            <a class="cust_button cust_button-mat btn--5">
+                                <div class="cust_btn-text"><span>SEE MORE</span> <i class="fas fa-long-arrow-alt-right"></i></div>
+                            </a>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php require_once('inc/contact.php'); ?>
+    <?php require_once('inc/footer.php'); ?>
+    <script type="application/javascript" src="js/owl.carousel.js"></script>
+    <script type="application/javascript">
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+            margin: 10,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        })
+
+    </script>
